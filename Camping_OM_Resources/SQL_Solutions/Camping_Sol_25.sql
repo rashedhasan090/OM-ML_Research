46085ebaedf5d8ae6f3a51d86a733b3d86197898
Md Rashedul Hasan <rashedhasan090@outlook.com>
2022-12-21 01:55:34 -0500

    camping

---
+++ Camping_OM_Resources/SQL_Solutions/Camping_Sol_25.sql
@@ -0,0 +1,111 @@
+-- CREATE DATABASE FOR /home/centos/orm_synthesizer/models/Camping/ImplSolution/Camping_Sol_23.sql
+
+USE Camping_Sol_23;
+--
+-- Table structure for table CampingTripScoutAssociation
+--
+
+CREATE TABLE `CampingTripScoutAssociation` (
+`TripId` int NOT NULL,
+`ScoutId` int NOT NULL,
+KEY `FK_CampingTripScoutAssociation_TripId_idx` (`TripId`),
+KEY `FK_CampingTripScoutAssociation_ScoutId_idx` (`ScoutId`),
+PRIMARY KEY (`TripId`,`ScoutId`)
+);
+
+--
+-- Table structure for table BadgeSpecification
+--
+
+CREATE TABLE `BadgeSpecification` (
+`BadgeName` varchar(64),
+`BadgeSpecificationId` int NOT NULL,
+PRIMARY KEY (`BadgeSpecificationId`)
+);
+
+--
+-- Table structure for table WealthyScout
+--
+
+CREATE TABLE `WealthyScout` (
+`Name` varchar(64),
+`DonationDate` int,
+`DonationAmount` int,
+`Joined` int,
+`DOB` int,
+`ScoutId` int NOT NULL,
+PRIMARY KEY (`ScoutId`)
+);
+
+--
+-- Table structure for table CampSite
+--
+
+CREATE TABLE `CampSite` (
+`Location` varchar(64),
+`SiteID` int NOT NULL,
+PRIMARY KEY (`SiteID`)
+);
+
+--
+-- Table structure for table ScoutBadgeAwardedAssociation
+--
+
+CREATE TABLE `ScoutBadgeAwardedAssociation` (
+`ScoutId` int NOT NULL,
+`AwardId` int NOT NULL,
+KEY `FK_ScoutBadgeAwardedAssociation_ScoutId_idx` (`ScoutId`),
+KEY `FK_ScoutBadgeAwardedAssociation_AwardId_idx` (`AwardId`),
+PRIMARY KEY (`ScoutId`,`AwardId`)
+);
+
+--
+-- Table structure for table CampingTrip
+--
+
+CREATE TABLE `CampingTrip` (
+`EndDate` int,
+`StartDate` int,
+`TripId` int NOT NULL,
+`SiteID` int,
+KEY `FK_CampingTrip_SiteID_idx` (`SiteID`),
+PRIMARY KEY (`TripId`)
+);
+
+--
+-- Table structure for table BadgeAwarded
+--
+
+CREATE TABLE `BadgeAwarded` (
+`BadgeSpecificationId` int,
+`Date` int,
+`AwardId` int NOT NULL,
+KEY `FK_BadgeAwarded_BadgeSpecificationId_idx` (`BadgeSpecificationId`),
+PRIMARY KEY (`AwardId`)
+);
+
+--
+-- Table structure for table Scout
+--
+
+CREATE TABLE `Scout` (
+`Name` varchar(64),
+`Joined` int,
+`DOB` int,
+`ScoutId` int NOT NULL,
+PRIMARY KEY (`ScoutId`)
+);
+
+ALTER TABLE `CampingTripScoutAssociation`
+  ADD CONSTRAINT `FK_CampingTripScoutAssociation_TripId` FOREIGN KEY (`TripId`) REFERENCES `CampingTrip` (`TripId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_CampingTripScoutAssociation_ScoutId` FOREIGN KEY (`ScoutId`) REFERENCES `Scout` (`ScoutId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `ScoutBadgeAwardedAssociation`
+  ADD CONSTRAINT `FK_ScoutBadgeAwardedAssociation_ScoutId` FOREIGN KEY (`ScoutId`) REFERENCES `Scout` (`ScoutId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_ScoutBadgeAwardedAssociation_AwardId` FOREIGN KEY (`AwardId`) REFERENCES `BadgeAwarded` (`AwardId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `CampingTrip`
+  ADD CONSTRAINT `FK_CampingTrip_SiteID` FOREIGN KEY (`SiteID`) REFERENCES `CampSite` (`SiteID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `BadgeAwarded`
+  ADD CONSTRAINT `FK_BadgeAwarded_BadgeSpecificationId` FOREIGN KEY (`BadgeSpecificationId`) REFERENCES `BadgeSpecification` (`BadgeSpecificationId`) ON DELETE CASCADE ON UPDATE CASCADE;
