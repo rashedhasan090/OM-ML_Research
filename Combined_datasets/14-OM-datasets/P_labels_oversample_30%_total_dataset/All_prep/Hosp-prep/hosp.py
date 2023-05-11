e9616872796889251d98e514ba392f140e28c2ad
Md Rashedul Hasan <rashedhasan090@outlook.com>
2023-05-10 19:43:10 -0500

    Notebook updates

---
+++ Combined_datasets/14-OM-datasets/P_labels_oversample_30%_total_dataset/All_prep/Hosp-prep/hosp.py
@@ -0,0 +1,25 @@
+from imblearn.over_sampling import RandomOverSampler
+from collections import Counter
+
+import pandas as pd
+df = pd.read_csv('Hosp30prep.csv')
+
+#Split our data into features(X) and labels(y)
+X=df.drop('OM_Regular',axis=1)
+y=df['OM_Regular']
+
+
+print("Original Count")
+
+print(y.value_counts())
+
+from imblearn.over_sampling import RandomOverSampler
+ros = RandomOverSampler(sampling_strategy="not majority") # String
+X_res, y_res = ros.fit_resample(X, y)
+
+
+print("Oversampled Count")
+
+print(y_res.value_counts())
+
+y_res.to_csv('hosp_oversample_3.csv')
