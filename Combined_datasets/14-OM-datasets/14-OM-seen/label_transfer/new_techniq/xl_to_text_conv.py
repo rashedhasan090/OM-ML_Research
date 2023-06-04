5a0e5784274dc02fdbadfb56a6807094eea09f28
Md Rashedul Hasan <rashedhasan090@outlook.com>
2023-06-04 15:46:40 -0500

    notebook changes

---
+++ Combined_datasets/14-OM-datasets/14-OM-seen/label_transfer/new_techniq/xl_to_text_conv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Path to the Excel file
+excel_file = "14_OM_label_nospace.xlsx"
+
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel(excel_file)
+
+# Select the desired columns
+selected_columns = ['OM_Regular', 'OM_Prediction']
+df_selected = df[selected_columns]
+
+# Convert the DataFrame to a tab-separated text file
+output_file = "14_OM_label_nospace.txt"
+df_selected.to_csv(output_file, sep='\t', index=False)
+
+print(f"Excel file '{excel_file}' converted to '{output_file}'.")
---
+++ Combined_datasets/14-OM-datasets/14-OM-seen/label_transfer/new_techniq/xl_to_text_conv_2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Path to the Excel file
+excel_file = "ecom.xlsx"
+
+# Read the Excel file into a pandas DataFrame
+df = pd.read_excel(excel_file)
+
+# Select the desired columns
+selected_columns = ['OM_Regular']
+df_selected = df[selected_columns]
+
+# Convert the DataFrame to a tab-separated text file
+output_file = "ecom.txt"
+df_selected.to_csv(output_file, sep='\t', index=False)
+
+print(f"Excel file '{excel_file}' converted to '{output_file}'.")
